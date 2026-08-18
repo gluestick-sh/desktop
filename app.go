cac50e1395db7ecae89e9091cac863e783ac8d90
xuchao0903 <xuchao0903@gmail.com>
2026-08-18 14:41:35 +0800

    chore(desktop): bump to 0.1.23 and core to v0.1.12

--- app.go
+++ app.go
@@ -18,7 +18,7 @@ import (
 // These may be overridden at link time via:
 // -ldflags "-X gluestick.sh/desktop.Version=..." etc.
 var (
-	Version = "0.1.22"
+	Version = "0.1.23"
 	Commit  = "none"
 	Date    = "unknown"
 )
