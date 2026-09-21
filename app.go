a724a91c48346ceba30b1ce61e7b1b8effbd6af2
xuchao0903 <xuchao0903@gmail.com>
2026-09-21 16:09:12 +0800

    chore: bump desktop version to 0.1.24 and ignore .cursorrules

--- app.go
+++ app.go
@@ -18,7 +18,7 @@ import (
 // These may be overridden at link time via:
 // -ldflags "-X gluestick.sh/desktop.Version=..." etc.
 var (
-	Version = "0.1.23"
+	Version = "0.1.24"
 	Commit  = "none"
 	Date    = "unknown"
 )
